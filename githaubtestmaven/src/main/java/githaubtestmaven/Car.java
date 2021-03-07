b0640c167d32b066595f81777a63c570a4f41d25
dpolonsky <dimitri.polonsky@f24.com>
2021-03-07 19:41:15 +0100

    init commit

---
+++ githaubtestmaven/src/main/java/githaubtestmaven/Car.java
@@ -0,0 +1,10 @@
+package githaubtestmaven;
+
+public class Car extends Vehicle {
+
+    public Car() {
+
+        // TODO Auto-generated constructor
+    }
+
+}
---
+++ githaubtestmaven/src/main/java/githaubtestmaven/MyMainClass.java
@@ -0,0 +1,11 @@
+package githaubtestmaven;
+
+public class MyMainClass {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "test" );
+
+    }
+
+}
